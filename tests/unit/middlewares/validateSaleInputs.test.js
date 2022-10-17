6991bc1b37bc6b903496573102e84425be10ceb2
Pedro Papini <ppapinia@gmail.com>
2022-10-17 16:58:44 -0300

    Ajusta importação nos testes

--- tests/unit/middlewares/validateSaleInputs.test.js
+++ tests/unit/middlewares/validateSaleInputs.test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
-const validateNewSaleInput = require('../../../src/middlewares/validateNewSaleInput');
+const validateSaleInput = require('../../../src/middlewares/validateSaleInput');
 
 const { expect } = chai;
 chai.use(sinonChai);
@@ -14,7 +14,7 @@ describe('O middleware de validação de nova venda:', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
 
-    await validateNewSaleInput(req, res);
+    await validateSaleInput(req, res);
 
     expect(res.status).to.have.been.calledWith(400);
     expect(res.json).to.have.been.calledWith({ message: '"productId" is required' });
@@ -27,7 +27,7 @@ describe('O middleware de validação de nova venda:', function () {
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns();
 
-    await validateNewSaleInput(req, res);
+    await validateSaleInput(req, res);
 
     expect(res.status).to.have.been.calledWith(400);
     expect(res.json).to.have.been.calledWith({ message: '"quantity" is required' });
